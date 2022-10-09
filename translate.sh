13512719ea0f87b4ca095e1dda10e7931166b484
sapzipf <yannick.zipf@icloud.com>
2022-10-09 12:15:03 +0200

    chore: correct pot file name for translation

--- translate.sh
+++ translate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ../../tools/i18n/trunk
 php makepot.php wp-plugin ../../../ergebnisse-h4a-free/plugin/results-for-handball4all
-mv results-h4a.pot ../../../ergebnisse-h4a-free/plugin/results-for-handball4all/languages/results-h4a.pot
+mv results-for-handball4all.pot ../../../ergebnisse-h4a-free/plugin/results-for-handball4all/languages/results-h4a.pot
